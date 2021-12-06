aab35a218c30fe7c5214f59cc3db92c90d7c80d8
Carlos Duarte <carlosnovaduarte@outlook.com>
2021-12-06 16:33:41 +0000

    Add backend connection to bioportal

--- backend/routes/infectious.js
+++ backend/routes/infectious.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const SparqlClient = require('sparql-http-client');
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-    res.send("Infectious Diseases endpoint")
-});
+const dbpediaEndpointUrl = "https://dbpedia.org/sparql";
+const bioportalEndpointUrl = "http://sparql.bioontology.org/sparql/"
+const bioportalApiKey = "7f190679-605d-41f2-89de-572af1f28205" // deveria estar num .env
+
+router.get('/', async (req, res) => {
+    console.log("Hi!");
+    var query_string = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#>\n" +
+    "SELECT ?ont ?name ?acr " +
+    "WHERE { " +
+    "	?ont a omv:Ontology .  " +
+    "	?ont omv:acronym ?acr ." +
+    "	?ont omv:name ?name . " +
+    " }";
+
+    await axios({
+        url: '/sparql?query=' + encodeURIComponent(query_string) + "&apikey=" + encodeURIComponent(bioportalApiKey),
+        baseURL: 'http://sparql.bioontology.org',
+        port: 8080,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+    }).then((res) => {
+        console.log(res.data.results);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    res.send("Hi!");
+    });
 
 module.exports = router;
