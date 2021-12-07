bc23e7f6ebca60965a84d99d8f191636f45209b0
Carlos Duarte <carlosnovaduarte@outlook.com>
2021-12-07 15:30:19 +0000

    Add backend connection to DBpedia

--- backend/routes/infectious.js
+++ backend/routes/infectious.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const SparqlClient = require('sparql-http-client');
 const axios = require('axios');
 
-const dbpediaEndpointUrl = "https://dbpedia.org/sparql";
-const bioportalEndpointUrl = "http://sparql.bioontology.org/sparql/"
+const dbpediaBaseUrl = "https://live.dbpedia.org";
+const bioportalBaseUrl = "http://sparql.bioontology.org"
 const bioportalApiKey = "7f190679-605d-41f2-89de-572af1f28205" // deveria estar num .env
 
-router.get('/', async (req, res) => {
-    console.log("Hi!");
+router.get('/dbpedia', async (req, res) => {
+    var query_string = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
+
+    await axios({
+        url: '/sparql?' + 'default-graph-uri=http%3A%2F%2Fdbpedia.org' + '&query=' + encodeURIComponent(query_string),
+        baseURL: dbpediaBaseUrl,
+        port: 443,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        }
+    }).then((dbpedia_res) => {
+        return res.json(dbpedia_res.data);
+    }).catch((err) => {
+        return res.json(err);
+    })
+});
+
+router.get('/bioportal', async (req, res) => {
     var query_string = "PREFIX omv: <http://omv.ontoware.org/2005/05/ontology#>\n" +
     "SELECT ?ont ?name ?acr " +
     "WHERE { " +
@@ -19,19 +35,17 @@ router.get('/', async (req, res) => {
 
     await axios({
         url: '/sparql?query=' + encodeURIComponent(query_string) + "&apikey=" + encodeURIComponent(bioportalApiKey),
-        baseURL: 'http://sparql.bioontology.org',
+        baseURL: bioportalBaseUrl,
         port: 8080,
         method: 'GET',
         headers: {
             'Accept': 'application/json',
         },
-    }).then((res) => {
-        console.log(res.data.results);
+    }).then((bioportal_res) => {
+        return res.send(bioportal_res.data);
     }).catch((err) => {
-        console.log(err);
+        return res.json(err);
     });
-
-    res.send("Hi!");
-    });
+});
 
 module.exports = router;
