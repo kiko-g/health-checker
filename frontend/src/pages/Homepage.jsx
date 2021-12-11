d4e991862a14176b4e821a38e3275a8cf9a2c17b
Nokas <up201605335@fe.up.pt>
2021-12-11 16:41:42 +0000

    add basic routing

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Hero from "../components/Hero"
 import Search from "../components/Search"
 
